0cc4f7dd968f461c6a31c2710763e86afff52fc0
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-09-15 18:25:15 -0400

    destroy and create method

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,4 +4,30 @@ class AlbumsController < ApplicationController
         albums = Album.all
         render json: albums
     end
+
+    def create
+        # album = Album.new(album_params)
+        # if album.save
+        #     render json: album
+        # else
+        #     render json: {error: "album not saved successfully" }
+        # end
+        album = Album.create(album_params)
+        render json: album
+    end
+
+    def destroy 
+
+        album = Album.find_by_id(params[:id])
+        album.destroy
+        render json: {message: "#{album.name} was deleted."}
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:name, :artist, :genre, :image, :description)
+        # byebug
+        # params.permit(:name, :artist, :genre, :image, :description)
+    end
 end
